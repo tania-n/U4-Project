7a2579fa6345b711b6c375d87fdf339b0f03e814
tania-n <137098@epsstudents.org>
2024-12-05 14:10:02 -0500

    Update main.java

--- main.java
+++ main.java
@@ -7,12 +7,10 @@ public class Main {
     Scanner text = new Scanner(file); 
 
     String str = text.nextLine();
-    
     System.out.println("Top Streams of 2016");
     int i = 1; 
     // while loop goes through each line for each song 
     while(text.hasNextLine()){ 
-        String final1 = ""; 
         str = text.nextLine(); 
         // finds the index of each "|" symbol 
         int index1 = str.indexOf("|");
@@ -20,20 +18,21 @@ public class Main {
         int index3 = str.indexOf("|", index2 + 1); 
         int index4 = str.indexOf("|", index3 + 1); 
         int index5 = str.indexOf("|", index4 + 1); 
+        // saves info between index 1 and 2 to title 
+        String title = str.substring(index1 + 1, index2); 
+        // saves info between index 2 and 3 to artist 
+        String artist = str.substring(index2 + 1, index3); 
+        // saves info between index 3 and 4 to album 
+        String album = str.substring(index3 + 1, index4); 
         // saves the date the song was released 
-        String substring1 = str.substring(index5 + 1); 
-        // checks if 2016 is in the string
-        if(substring1.indexOf("2016") != -1){ 
+        String date = str.substring(index5 + 1); 
+        // checks if 2016 is in the string 
+        if(date.indexOf("2016") != -1){ 
+            // saves song into song1 
+            Song song1 = new Song(title, artist, album, 2016); 
             System.out.println(i); 
-            // saves info between index 1 and 2 to title 
-            String title = str.substring(index1 + 1, index2); 
-            // saves info between index 2 and 3 to artist 
-            String artist = str.substring(index2 + 1, index3); 
-            // saves info between index 3 and 4 to album 
-            String album = str.substring(index3 + 1, index4); 
-            final1 += "Artist: " + artist + "\n" + "Title: " + title + "\n" + "Album: " + album; 
-            System.out.println(final1); 
-            System.out.println();
+            System.out.println(song1);
+            System.out.println(); 
             // increases i by 1 after each loop 
             i++; 
         }
